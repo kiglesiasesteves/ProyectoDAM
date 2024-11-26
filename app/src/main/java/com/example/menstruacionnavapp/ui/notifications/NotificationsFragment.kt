3a96f82ab41357170f3b97373a3214b993515879
keyla <kiglesiasesteves@danielcastelao.org>
2024-11-26 14:20:25 +0100

    MAL

--- app/src/main/java/com/example/menstruacionnavapp/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/menstruacionnavapp/ui/notifications/NotificationsFragment.kt
@@ -8,6 +8,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.menstruacionnavapp.databinding.FragmentNotificationsBinding
+import com.kizitonwose.calendar.compose.
+
 
 class NotificationsFragment : Fragment() {
 
