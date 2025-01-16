f05de5caf9a7a242fe9aa953cffd03789e28f874
keyla <kiglesiasesteves@danielcastelao.org>
2025-01-16 13:51:25 +0100

    APTO: Abre la nueva pestaña llamada Buscar Gimnasios

---
+++ app/src/main/java/com/example/menstruacionnavapp/ui/buscarGimnasios/GymFragment.kt
@@ -0,0 +1,11 @@
+package com.example.menstruacionnavapp.ui.buscarGimnasios
+
+import androidx.fragment.app.Fragment
+import com.example.menstruacionnavapp.databinding.FragmentGymsBinding
+
+class GymFragment : Fragment() {
+    // View binding para el fragmento
+    private var _binding: FragmentGymsBinding? = null
+    private val binding get() = _binding!!
+
+}
