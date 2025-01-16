c10df7b0a3095310f68461caba1a3107e50077b3
keyla <kiglesiasesteves@danielcastelao.org>
2025-01-16 14:19:42 +0100

    APTO: Añadimos MAPA GOOGLE MAPS

--- app/src/main/java/com/example/menstruacionnavapp/ui/buscarGimnasios/GymFragment.kt
+++ app/src/main/java/com/example/menstruacionnavapp/ui/buscarGimnasios/GymFragment.kt
@@ -1,11 +1,42 @@
 package com.example.menstruacionnavapp.ui.buscarGimnasios
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.menstruacionnavapp.R
 import com.example.menstruacionnavapp.databinding.FragmentGymsBinding
+import com.google.android.gms.maps.SupportMapFragment
 
 class GymFragment : Fragment() {
-    // View binding para el fragmento
     private var _binding: FragmentGymsBinding? = null
     private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val gymViewModel = ViewModelProvider(this).get(GymViewModel::class.java)
+
+        _binding = FragmentGymsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+
+        // Initialize the SupportMapFragment
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync { googleMap ->
+            // Do something with the googleMap
+        }
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
