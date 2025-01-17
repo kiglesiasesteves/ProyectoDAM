c3ba62750013b8fb454948c08dcf4dbafb18e9eb
keyla <kiglesiasesteves@danielcastelao.org>
2025-01-17 12:42:52 +0100

    APTO: cambio nombre a calendar

--- app/src/main/java/com/example/menstruacionnavapp/ui/calendario/CalendarFragment.kt
+++ app/src/main/java/com/example/menstruacionnavapp/ui/calendario/CalendarFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.menstruacionnavapp.databinding.FragmentDashboardBinding
 
-class DashboardFragment : Fragment() {
+class CalendarFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
 
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val calendarViewModel =
+            ViewModelProvider(this).get(CalendarViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        calendarViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
